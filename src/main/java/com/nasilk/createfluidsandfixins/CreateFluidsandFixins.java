15de51e27145d4921a68835298b6908eb6d190ee
Pebbbles <stevecostello175@gmail.com>
2026-05-02 15:35:24 -0700

    second commit

--- src/main/java/com/nasilk/createfluidsandfixins/CreateFluidsandFixins.java
+++ src/main/java/com/nasilk/createfluidsandfixins/CreateFluidsandFixins.java
@@ -32,18 +32,18 @@ import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(CreateFluidsandFixins.MODID)
+@Mod(CreateFluidsandFixins.MOD_ID)
 public class CreateFluidsandFixins {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "createfluidsandfixins";
+    public static final String MOD_ID = "createfluidsandfixins";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "createfluidsandfixins" namespace
-    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MOD_ID);
     // Create a Deferred Register to hold Items which will all be registered under the "createfluidsandfixins" namespace
-    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MOD_ID);
     // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "createfluidsandfixins" namespace
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MOD_ID);
 
     // Creates a new Block with the id "createfluidsandfixins:example_block", combining the namespace and path
     public static final DeferredBlock<Block> EXAMPLE_BLOCK = BLOCKS.registerSimpleBlock("example_block", BlockBehaviour.Properties.of().mapColor(MapColor.STONE));
