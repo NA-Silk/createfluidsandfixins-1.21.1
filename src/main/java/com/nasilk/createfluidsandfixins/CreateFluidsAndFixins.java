63f1d6681f106a4e4ed2167accc4e58124626bcc
NA-Silk <nicolasseda@gmail.com>
2026-05-04 00:38:08 -0400

    added end fluid extraction mixin

--- src/main/java/com/nasilk/createfluidsandfixins/CreateFluidsAndFixins.java
+++ src/main/java/com/nasilk/createfluidsandfixins/CreateFluidsAndFixins.java
@@ -4,18 +4,6 @@ import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
 
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
@@ -25,10 +13,6 @@ import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(CreateFluidsAndFixins.MOD_ID)
@@ -46,7 +30,7 @@ public class CreateFluidsAndFixins {
         modEventBus.addListener(this::commonSetup);
 
         // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (CreateFluidsandFixins) to respond directly to events.
+        // Note that this is necessary if and only if we want *this* class (CreateFluidsAndFixins) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
@@ -58,7 +42,7 @@ public class CreateFluidsAndFixins {
     }
 
     private void commonSetup(FMLCommonSetupEvent event) {
-
+        LOGGER.info("Create Fluids & Fixins loaded");
     }
 
     // Add the example block item to the building blocks tab
---
+++ src/main/java/com/nasilk/createfluidsandfixins/mixins/PipeExtractionMixin.java
@@ -0,0 +1,40 @@
+package com.nasilk.createfluidsandfixins.mixins;
+
+import com.simibubi.create.content.fluids.OpenEndedPipe;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.core.BlockPos;
+
+import net.neoforged.neoforge.fluids.FluidStack;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(OpenEndedPipe.class)
+public class PipeExtractionMixin {
+
+    @Shadow private Level world;
+    @Shadow private BlockPos outputPos;
+
+    @Inject(method = "removeFluidFromSpace", at = @At("HEAD"), cancellable = true)
+    private void removeFluidFromSpaceMixin(boolean simulate, CallbackInfoReturnable<FluidStack> cir) {
+
+        // Error check
+        if (world == null)
+            return;
+
+        // Check if in END and y <= 5
+        if (world.dimension() == Level.END && outputPos.getY() <= 5) {
+
+            // Debug message
+            System.out.println("Custom End fluid extraction triggered at " + outputPos);
+
+            // Return lava as if it was extracted
+            cir.setReturnValue(new FluidStack(Fluids.LAVA, 1000));
+        }
+    }
+}
